f6a54bc6bd848e49793e328003909d41d63d29c7
ThanagritB <thanagrit.ban@gmail.com>
2025-06-20 14:46:39 +0700

    - Implement .env.example. - Config TypeORM to connect DB in app.module.ts.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,34 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BooksModule } from './books/books.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    // Load .env and set it to global. Can use in every module (No need to import again).
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    // Connect database with data in .env.
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'postgres',
+        host: process.env.DB_HOST,
+        port: parseInt(process.env.DB_PORT || '5432', 10),
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        entities: [__dirname + '/**/*.entity.{ts,js}'],
+        synchronize: process.env.APP_ENV === 'dev', // It will create/update data schema automatically. (Use only dev!)
+        logging: true,
+      }),
+    }),
+
+    BooksModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
